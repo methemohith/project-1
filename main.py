a8743a9d551ab5bd6415f528fc5f9d7cc41e8f10
hva8055 <78602185+hva8055@users.noreply.github.com>
2022-10-31 12:28:26 +0530

    uplodidng python file

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print("hello this is collabrat")
+import pandas
+import matplotlib.pyplot as py
+from sklearn.linear_model import LinearRegression
+data = pandas.read_csv('cost.csv')
+d = data.describe()
+print(d)
+x = pandas.DataFrame(data, columns=['budget'])
+y = pandas.DataFrame(data, columns=['worldwide'])
+py.figure(figsize=(10, 6,))
+py.scatter(x, y, alpha=0.3)
+py.xlabel('budget')
+py.ylabel('world wide collection')
+py.xlim(150000000, 500000000)
+py.ylim(0, 3500000000)
+re = LinearRegression()
+re.fit(x, y)
+py.plot(x, re.predict(x), color='red', linewidth=2)
+py.show()
+
+sprint(re.score(x, y))
